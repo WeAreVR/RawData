5f7ab8410d2c90bfb55ef5a69c8eaf89c8a14ee4
Braeze <tobias0612@live.dk>
2021-10-22 15:15:04 +0200

    upload igen

---
+++ RAWDATA_2021_Livecode2-master/Client/ClientProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using Utillities;
+
+namespace Client
+{
+    
+    class ClientProgram
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new NetworkClient();
+
+            client.Connect("localhost", 5000);
+
+            var message = "hello";
+            client.Write(message);
+
+            var response = client.Read();
+
+            Console.WriteLine($"Server response '{response}'");
+
+        }
+    }
+}
