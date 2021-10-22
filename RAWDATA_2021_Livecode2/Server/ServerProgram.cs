69fd3268f822a2119464d92071b36f3ddd0e8a49
Thomas1Mortensen <47951101+Thomas1Mortensen@users.noreply.github.com>
2021-10-22 14:05:23 +0200

    Add files via upload

---
+++ RAWDATA_2021_Livecode2/Server/ServerProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Utillities;
+
+namespace Server
+{
+    class ServerProgram
+    {
+        static void Main(string[] args)
+        {
+            var server = new TcpListener(IPAddress.Loopback, 5000);
+            server.Start();
+            Console.WriteLine("Server started");
+
+            while (true)
+            {
+                var client = new NetworkClient(server.AcceptTcpClient());
+                Console.WriteLine("Client accepted");
+
+                var message = client.Read();
+
+                Console.WriteLine($"Client message '{message}'");
+
+                client.Write(message.ToUpper());
+            }
+
+        }
+    }
+}
