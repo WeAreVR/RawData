bbc3fee02819b0f86da0dd96e3a8950861720524
Louise Lundegaard <71316864+RainbowLampp@users.noreply.github.com>
2021-10-27 17:40:26 +0200

    Min Git virker ikke :(
    Den virker nu :)

---
+++ Assignment4/Assignment4/NorthwindContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Assignment4.Domain;
+
+namespace Assignment4
+{
+    
+    public class NorthwindContext : DbContext
+    {
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseNpgsql("host=localhost;db=Northwind;uid=postgres;pwd=ctj66yjr");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().ToTable("categories");
+            modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
+            modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
+            modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
+
+
+            modelBuilder.Entity<Product>().ToTable("products");
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
+            modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
+            modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
+
+        }
+    }
+}
