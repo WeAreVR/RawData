3557ee5c76fdc5fe5d82420de8f4d3a405a208e5
Louise Lundegaard <louiselundegaard12@yahoo.dk>
2021-10-27 17:46:50 +0200

    Test
    Måske virker det

--- Assingment4/Assignment4/program.cs
+++ Assingment4/Assignment4/program.cs
@@ -12,47 +12,50 @@ namespace Assignment4
         {
             var dataService = new DataService();
 
-            foreach(var category in dataService.GetCategories())
+            foreach (var category in dataService.GetCategories())
             {
                 Console.WriteLine(category);
             }
         }
 
-        static void FirstTake()
+
+       /* static void FirstTake()
         {
             var ctx = new NorthwindContext();
 
-            //var categories = ctx.Categories;
+            var categories = ctx.Categories;
 
-            //foreach (var category in categories)
-            //{
-            //    Console.WriteLine(category);
-            //}
+            foreach (var category in categories)
+            {
+                Console.WriteLine(category);
+            }
 
-            //var cat = new Category
-            //{
-            //    Id = 101,
-            //    Name = "fdfjslfdjfk",
-            //    Description = "blah blah"
-            //};
+            var cat = new Category
+            {
+                Id = 101,
+                Name = "fdfjslfdjfk",
+                Description = "blah blah"
+            };
 
-            //ctx.Categories.Add(cat);
-            //ctx.SaveChanges();
+            ctx.Categories.Add(cat);
+            ctx.SaveChanges();
+            
+            //var test = ctx.Categories.Find(101);
 
-            //var cat = ctx.Categories.Find(101);
+            Console.WriteLine("hey");
+            
+            cat.Name = "New name";
+            ctx.SaveChanges();
 
-            //cat.Name = "New name";
-            //ctx.SaveChanges();
-
-            //ctx.Categories.Remove(ctx.Categories.Find(101));
-            //ctx.SaveChanges();
+            ctx.Categories.Remove(ctx.Categories.Find(101));
+            ctx.SaveChanges();
 
             var products = ctx.Products.Include(x => x.Category);
-
+            
             foreach (var product in products)
             {
                 Console.WriteLine(product);
             }
-        }
+        }*/
     }
 }
