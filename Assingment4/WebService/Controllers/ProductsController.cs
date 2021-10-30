206d07ca295fd85c1a0e85f29a9de816b0d8289a
Louise Lundegaard <louiselundegaard12@yahoo.dk>
2021-10-30 11:02:29 +0200

    Added Controller and ViewModels for Products

---
+++ Assingment4/WebService/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+using Assignment4;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebService.ViewModels;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : Controller
+    {
+
+        IDataService _dataService;
+        LinkGenerator _linkGenerator;
+
+        public ProductsController(IDataService dataService, LinkGenerator linkGenerator)
+        {
+            _dataService = dataService;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _dataService.GetProducts();
+
+            return Ok(products.Select(x => GetProductViewModel(x)));
+        }
+
+        // api/products/id
+        [HttpGet("{id}", Name = nameof(GetProduct))]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _dataService.GetProduct(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ProductViewModel model = GetProductViewModel(product);
+
+            return Ok(model);
+        }
+
+        
+        private ProductViewModel GetProductViewModel(Assignment4.Domain.Product product)
+        {
+            return new ProductViewModel
+            {
+
+                Url = GetUrl(product),
+                Name = product.Name,
+                CategoryId = product.CategoryId
+            };
+        }
+
+        private string GetUrl(Assignment4.Domain.Product product)
+        {
+            return _linkGenerator.GetUriByName(HttpContext, nameof(GetProduct), new { product.Id });
+        }
+
+
+    }
+}
