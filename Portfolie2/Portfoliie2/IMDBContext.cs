2959951eabf5f13c0f082326a8781e72cc1027ba
Thomas1Mortensen <47951101+Thomas1Mortensen@users.noreply.github.com>
2021-11-07 11:59:25 +0100

    virker nu med wep

--- Portfolie2/Portfoliie2/IMDBContext.cs
+++ Portfolie2/Portfoliie2/IMDBContext.cs
@@ -32,7 +32,7 @@ namespace Portfolie2
 
             optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
             optionsBuilder.EnableSensitiveDataLogging();
-            optionsBuilder.UseNpgsql("host=localhost;db=imdb;uid=postgres;pwd=Anders18");
+            optionsBuilder.UseNpgsql("host=localhost;db=imdb_database;uid=postgres;pwd=");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
