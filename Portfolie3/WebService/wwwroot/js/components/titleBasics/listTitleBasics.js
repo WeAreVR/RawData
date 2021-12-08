1659a02a10e594bcc49b632c47ee4b39b10f0d7b
Thomas1Mortensen <47951101+Thomas1Mortensen@users.noreply.github.com>
2021-12-08 12:04:49 +0100

    det virker ikke jeg ødelage det -_-

--- Portfolie3/WebService/wwwroot/js/components/titleBasics/listTitleBasics.js
+++ Portfolie3/WebService/wwwroot/js/components/titleBasics/listTitleBasics.js
@@ -1,9 +1,13 @@
-﻿
+﻿const { data } = require("jquery");
+
 define(['knockout', 'dataService', 'postman'], function (ko, ds, postman) {
     return function (params) {
         let currentComponent = ko.observable("list");
         let pageSize = [5, 10, 15, 100];
+        let selectedPageSize = ko.observable([10]);
         let currentView = ko.observable("list-titles");
+        let prev = ko.observable();
+        let next = ko.observable();
 
         let titleBasics = ko.observableArray([]);
         let selectId= ko.observable();
@@ -24,10 +28,26 @@ define(['knockout', 'dataService', 'postman'], function (ko, ds, postman) {
         }
 
         let commentSection = () => postman.publish("changeView", "list-comments");
+        
+        let getData = url => {
+            dataService.getTitleBasics(url, data => {
+                prev(data.prev || undefined);
+                next(data.next || undefined);
+                titleBasics(data.items)
+            })
+        }
 
+        selectedPageSize.subscribe(() => {
+            var size = selectedPageSize()[0];
+            getData(dataService.getTitleBasicsWithPageSize(size));
+        });
 
+        getData();
 
         return {
+            prev,
+            next,
+            selectedPageSize,
             pageSize,
             currentComponent,
             currentView,
